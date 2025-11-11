4f5a0177e5f9e151746e1e5d0bb89999deab5854
rajnavami <rajn@students.uni-marburg.de>
2025-11-11 11:24:14 +0000

    corrected dinov3 import

--- models/dinov3/model/__init__.py
+++ models/dinov3/model/__init__.py
@@ -11,7 +11,7 @@ from typing import Union
 import torch
 import torch.nn as nn
 
-from dinov3.layers.fp8_linear import convert_linears_to_fp8
+from ..layers.fp8_linear import convert_linears_to_fp8
 
 from . import vision_transformer as vits
 
