6ace7e24c48b724a2c83f4406bf440c1fc401892
rajnavami <rajn@students.uni-marburg.de>
2026-01-15 19:27:58 +0000

    updated dinov3 with lora to increase performance

--- configs/base/base_utils.py
+++ configs/base/base_utils.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 
 def get_checkpoint_path(base_name, name):
-    ckpt_path = "checkpoint_DINOv3_lora_performance_enhance" #_dinov3
+    ckpt_path = "checkpoint_DINOv3_lora_performance_enhance" #_dinov2
     return ckpt_path
 
 
